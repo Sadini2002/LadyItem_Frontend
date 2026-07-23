d34d5d4a7e8464e3c929cfda81c44b6c2bfd582a
Sadini <jasingha2002sadninipunika@gamil.com>
2026-07-23 23:14:48 +0530

    change

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,13 @@
 import React from 'react'
-import { BrowserRouter as Router, Routes , Route } from 'react-router-dom'
-import './index.css'
-import Hero from './components/Hero'
-import Services from './components/Solution'
-import Narbar from './components/Navbar'
-import Footer from './components/Footer'  
-import About from './pages/About'
 
 
 function App() {
   return (
     <>
-     <Narbar/>
-    <Router>
-       <Narbar/>
-      <Routes>
-      <Route path='/'element={<Hero/>}/>
-      <Route path='/home' element={<Hero/>}/>
-      <Route path='/about' element={<About/>}/>
-      <Route path='/services' element={<Services/>}/>
-      <Route path='/contact' element={<Footer/>}/>
-    </Routes>
-    <About/>
-    <Services/>
-    <Footer/>
-    </Router>
-  
+     
+  <h1 className="text-4xl font-bold text-blue-600">
+      Welcome to My Website
+    </h1>
    
    
       
